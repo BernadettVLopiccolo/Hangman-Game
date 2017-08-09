2fae4c1a8cb54e670ad74a4914da63fcaadbad1b
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-08-09 11:25:37 -0700

    Added to README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hangman-Game
+
+In this assignment, I created a Hangman Game. These apps will run in the browser, and feature dynamically updated HTML powered by JavaScript code.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var words = ["oars", "amrap", "deadlift", "namaste", "marathon"];
 var remainGuess = 10;
 
 //computer picks a random word
-var compPick = Math.floor(Math.random() *5) + 1;
+var compPick = Math.floor(Math.random() * 5);
 
 //setting up an empty bucket for guesses
 var previous_guesses = {}
@@ -13,91 +13,94 @@ var wordArray = new Array;
 //new var that picks a random word from words
 //print out word
 var compWords = words[compPick];
-    console.log("sportWords",compWords);
+console.log("sportWords", compWords);
 
 //splits all od the values in words by the quote 
 var blankWordsArray = compWords.split("");
-    console.log("Array of sports words name",blankWordsArray);
+console.log("Array of sports words name", blankWordsArray);
 
 //length of the word
 var wordsLength = blankWordsArray.length;
-    console.log(wordsLength);
+console.log(wordsLength);
 
-    //sets up the underscore based on the word length
-    for(var i = 0; i < wordsLength; i++) {
-        blankWordsArray[i] = " _ ";
-        document.getElementById("blanks").innerHTML += blankWordsArray[i];
-       }
+//sets up the underscore based on the word length
+for (var i = 0; i < wordsLength; i++) {
+    blankWordsArray[i] = " _ ";
+    document.getElementById("blanks").innerHTML += blankWordsArray[i];
+}
 
-        
-        compWordsArray = compWords.split("");
-        console.log("reset sport words array",compWordsArray);
 
-        //prints out the key user pressed
-        document.onkeyup = function(event) {
-            console.log("Picked key", event.key);
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-            document.getElementById("userGuess").innerHTML += " " + userGuess;
+compWordsArray = compWords.split("");
+console.log("reset sport words array", compWordsArray);
 
-         //new addition, js nonsence   
-           //for(i = 0; i < previous_guesses.length; i++) {
-            //if(userGuess == previous_guesses[i]) {
-                //previouslyEntered = true;
-            //}
-           //}
-            //if(!previouslyEntered) {
-                //previous_guesses.push(userGuess);
-                //for (i =0; i < wordArray.length; i++) {
-                    //found = true;
-                    //document.appendChild(userGuess);
-                //}
+//prints out the key user pressed
+document.onkeyup = function(event) {
+    console.log("Picked key", event.key);
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    document.getElementById("userGuess").innerHTML += " " + userGuess;
 
-            //}
-            //if(found) 
+    //new addition, js nonsence   
+    //for(i = 0; i < previous_guesses.length; i++) {
+    //if(userGuess == previous_guesses[i]) {
+    //previouslyEntered = true;
+    //}
+    //}
+    //if(!previouslyEntered) {
+    //previous_guesses.push(userGuess);
+    //for (i =0; i < wordArray.length; i++) {
+    //found = true;
+    //document.appendChild(userGuess);
+    //}
 
-            //if letter is guessed  wrong, you loose a guess
-            remainGuess--;
-        //setting up max guesses to 10    
-        if(remainGuess < 0) {
-            remainGuess = 10;
-            if (remainGuess = 0) {
+    //}
+    //if(found) 
+
+    //if letter is guessed  wrong, you loose a guess
+    remainGuess--;
+    //setting up max guesses to 10    
+    if (remainGuess < 0) {
+        remainGuess = 10;
+        if (remainGuess === 0) {
 
             console.log("game over", remainGuess);
-              }
-            }
-        //shows remaining guesses
-        document.getElementById("remaining").innerHTML = remainGuess;
-             
-        
-       for(var l = 0; l <= wordsLength; l++) {
-            blankWordsArray[i] = " _ ";
-          if(userGuess === compWordsArray[l]){
-                // changed [l] to [i], = or ===       
-                blankWordsArray[i] === compWordsArray[l];
-
-                //alert(entered_guesses);
-                console.log("Picked key",userGuess);
-                console.log("computer's sports words",compWordsArray);
-                console.log("it's a match");
-                
-            }
-
-
-            else {
-                console.log(userGuess);
-                //console.log(compWordsArray[l]);
-            }
         }
+    }
+    //shows remaining guesses
+    document.getElementById("remaining").innerHTML = remainGuess;
 
+    //fix this part
+    for (var l = 0; l <= compWordsArray.length; l++) {
+        blankWordsArray[i] = " _ ";
+        if (userGuess === compWordsArray[l]) {
+            // changed [l] to [i], = or ===        
+            blankWordsArray[i] = compWordsArray[l];
 
-          if(previous_guesses == blankWordsArray[i]) {
+            //alert(entered_guesses);
+            console.log('blankWordsArray', blankWordsArray);
+            console.log("Picked key", userGuess);
+            console.log("computer's sports words", compWordsArray);
+            console.log("it's a match");
 
-            words += entered_guesses;
-            }
-            else {
-             words += " ";
-          }
-
+        } else {
+            console.log(userGuess);
+            //console.log(compWordsArray[l]);
         }
-        
-        
+    }
+
+    //sets up the underscore based on the word length
+    document.getElementById("blanks").innerHTML = '';
+    console.log('blankWordsArray2', blankWordsArray);
+    
+    for (var j = 0; j < blankWordsArray.length; j++) {
+        document.getElementById("blanks").innerHTML += blankWordsArray[i];
+    }
+
+
+    if (previous_guesses === blankWordsArray[i]) {
+
+        words += entered_guesses;
+    } else {
+        words += " ";
+    }
+
+}
